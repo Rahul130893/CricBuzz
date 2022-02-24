c1af4e2f9062a45ddc40d10463f1fe957ff0007c
Rahul130893 <93374760+Rahul130893@users.noreply.github.com>
2022-02-24 18:49:08 +0530

    some chnges in login page

--- scripts/login.js
+++ scripts/login.js
@@ -7,9 +7,13 @@ function getOtp(){
     if(inputEmail!=""){
         let otp=Math.floor(Math.random()*1000000)
        localStorage.setItem("sent-otp", otp)
-       alert("otp send to your Email")
+       alert("O tp send to your Email")
+       window.location.href="otp.html"
 
     }
  
+}
+function gotoHome(){
+    window.location.href="index.html"
 }
 
